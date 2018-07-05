e81e0d95b1bea2ca11a21be1bbce7494cc27cab8
alsq-gravity <asquassabia@jackhenry.com>
2018-07-04 20:52:27 -0600

    some chapter 5 exercises

--- src/main/scala/org/exercises/fpis/ch5/Chapter5.scala
+++ src/main/scala/org/exercises/fpis/ch5/Chapter5.scala
@@ -1,6 +1,7 @@
 package org.exercises.fpis.ch5
 
 import org.exercises.fpis.ch3.{C3Cons, C3List, C3Nil, Chapter3L}
+import org.exercises.fpis.ch4.{C4None, C4Option, C4Some}
 
 import scala.annotation.tailrec
 
@@ -18,7 +19,7 @@ sealed trait C5Stream[+A] {
   }
 
   // 5.2
-  def take(n: Int): (C5Stream[A], C3List[A]) = {
+  def takeL(n: Int): (C5Stream[A], C3List[A]) = {
     def leftovers_rtaken: (C5Stream[A], C3List[A]) = {
       @tailrec
       def go(depleting: C5Stream[A], acc: C3List[A], count: Int): (C5Stream[A], C3List[A]) = depleting match {
@@ -36,6 +37,12 @@ sealed trait C5Stream[+A] {
     (leftovers,Chapter3L.reverse(rtaken))
   }
 
+  def take(n: Int): C5Stream[A] = this match {
+    case C5Empty => C5Empty
+    case _ if n == 0 => C5Empty
+    case C5Cons(h, t) => C5Stream.cons(h(), t().take(n-1))
+  }
+
   def drop(n: Int): C5Stream[A] = {
     @tailrec
     def go(depleting: C5Stream[A], acc: C3List[A], count: Int): C5Stream[A] = depleting match {
@@ -49,8 +56,83 @@ sealed trait C5Stream[+A] {
     }
   }
 
+  // 5.3
+  def takeWhile(p: A => Boolean): C5Stream[A] = this match {
+    case C5Cons(at, ats) if p(at()) => C5Stream.cons[A](at(), ats().takeWhile(p))
+    case _ => C5Stream.empty
+  }
+
+  def foldRight[B](z: => B)(f: (A, => B) => B): B = this match {
+    case C5Cons(at, ats) => f(at(), ats().foldRight(z)(f))
+    case _ => z
+  }
+
+  // 5.4
+  def forAll(p: A => Boolean): Boolean = {
+    @tailrec
+    def go(depleting: C5Stream[A]): Boolean = depleting match {
+      case C5Empty => true
+      case C5Cons(at, _) if !p(at()) => false
+      case C5Cons(_, ats) => go(ats())
+    }
+    go(this)
+  }
+
+  // 5.5
+  def takeWhileR(p: A => Boolean): C5Stream[A] = {
+    def f(a: A, s: => C5Stream[A]): C5Stream[A] = if (p(a)) C5Stream.cons(a,s) else C5Stream.empty
+    foldRight(C5Stream.empty[A])(f)
+  }
+
+  // 5.6
+  def headOption: C4Option[A] = {
+    def frf(a: A, s: => C4Option[A]): C4Option[A] = this match {
+      case C5Empty => s
+      case _ => C4Some(a)
+    }
+    val frf1 : (A, => C4Option[A]) => C4Option[A] = frf
+    foldRight[C4Option[A]](C4None:C4Option[A])(frf1)
+  }
+
+  // 5.7
+  def map[B](f: A => B): C5Stream[B] = {
+    def frf(a: A, s: => C5Stream[B]): C5Stream[B] = C5Stream.cons[B](f(a),s)
+    foldRight[C5Stream[B]](C5Stream.empty[B])(frf)
+  }
+
+  def filter(p: A => Boolean): C5Stream[A] = {
+    def frf(a: A, s: => C5Stream[A]): C5Stream[A] = if(p(a)) C5Stream.cons(a,s) else s
+    foldRight[C5Stream[A]](C5Stream.empty[A])(frf)
+  }
+
+  def append[B>:A](as: C5Stream[B]): C5Stream[B] = {
+    def frf(a: A, s: => C5Stream[B]): C5Stream[B] = C5Stream.cons[B](a,s)
+    foldRight[C5Stream[B]](as)(frf)
+  }
+
+  def flatMap[B](f: A => C5Stream[B]): C5Stream[B] = {
+    def frf(a: A, s: => C5Stream[B]): C5Stream[B] = f(a).append[B](s)
+    foldRight[C5Stream[B]](C5Stream.empty[B])(frf)
+  }
+
+
+  // 5.13
+  def unfoldMap[B](f: A => B): C5Stream[B] = {
+    def next(aas: C5Stream[A]): C4Option[(B, C5Stream[A])] = aas.headOption.map(a => (f(a),aas.drop(1)))
+    C5Stream.unfold[B,C5Stream[A]](this)(next)
+  }
+  def unfoldTake(n: Int): C5Stream[A] = {
+    def next(aas: C5Stream[A], count: Int): C4Option[(A, (C5Stream[A],Int))] = aas.headOption.flatMap(a => if (count<n) C4Some((a,(aas.drop(1),count+1))) else C4None)
+    C5Stream.unfold[A,(C5Stream[A],Int)]((this,0))(z => next(z._1,z._2))
+  }
+  def unfoldTakeWhile(p: A => Boolean): C5Stream[A] = {
+    def next(aas: C5Stream[A]): C4Option[(A, C5Stream[A])] = aas.headOption.flatMap(a => if (p(a)) C4Some((a,aas.drop(1))) else C4None)
+    C5Stream.unfold[A,C5Stream[A]](this)(next)
+  }
+
 
 }
+
 case object C5Empty extends C5Stream[Nothing]
 case class C5Cons[+A](h: () => A, t: () => C5Stream[A]) extends C5Stream[A]
 
@@ -62,4 +144,47 @@ object C5Stream {
   }
   def empty[A]: C5Stream[A] = C5Empty
   def apply[A](as: A*): C5Stream[A] = if (as.isEmpty) empty else cons(as.head, apply(as.tail: _*))
+
+  // 5.8
+  def constant[A](a: A): C5Stream[A] = C5Stream.cons(a, constant(a))
+  // 5.9
+  def from(a: Int): C5Stream[Int] = C5Stream.cons[Int](a, C5Stream.from(a+1))
+  // 5.10
+  // (0) 1 1 2 3 5 ... f(n+1) = f(n) + f(n-1)
+  val fibSeqFrom1: C5Stream[Int] = {
+    def fibNext(fibN: Int, fibNmin1: Int): C5Stream[Int] = C5Stream.cons[Int](fibN, fibNext(fibN + fibNmin1, fibN))
+    fibNext(1,0)
+  }
+
+  // 5.11
+  def unfold[A,S](z:S)(f: S => C4Option[(A,S)]): C5Stream[A] = {
+    def next(aa:A, ss:S): C5Stream[A] = C5Stream.cons(aa,unfold(ss)(f))
+    f(z).map[C5Stream[A]]{case(a,s) => next(a,s)}.getOrElse(C5Stream.empty[A])
+  }
+
+  // 5.12
+  def unfoldOnes: C5Stream[Int] = unfold[Int,Unit](())((_: Unit) => C4Some((1,())))
+  def unfoldConst[A](const:A): C5Stream[A] = unfold[A,Unit](())((_: Unit) => C4Some((const,())))
+  def unfoldFrom(start:Int): C5Stream[Int] = unfold[Int,Int](start)((current: Int) => {
+    val nextStart = current+1
+    C4Some((current,nextStart))
+  })
+  def unfoldFibFrom2: C5Stream[Int] = {
+    def next(nMin1: Int, n: Int): C4Option[(Int, (Int, Int))] = C4Some((nMin1+n, (n, nMin1+n)))
+    unfold[Int, (Int, Int)]((0, 1))((curr: (Int, Int)) => next(curr._1, curr._2))
+  }
+
+  // 5.13
+  def c5zipWith[A,B,C](ina: C5Stream[A], inb: C5Stream[B])(f: (A,B) => C): C5Stream[C] = {
+    val maybeC: C4Option[C] = C4Option.map2(ina.headOption,inb.headOption)(f)
+    maybeC.fold(C5Stream.empty[C])((c:C) => C5Stream.cons[C](c,c5zipWith(ina.drop(1),inb.drop(1))(f)))
+  }
+  def unfoldZipAll[A,B](ina: C5Stream[A], inb: C5Stream[B]): C5Stream[(C4Option[A],C4Option[B])] = {
+    def next(aas:C5Stream[A], bbs:C5Stream[B]): C4Option[((C4Option[A], C4Option[B]), (C5Stream[A], C5Stream[B]))] = (aas.headOption, bbs.headOption) match {
+      case (C4None,C4None) => C4None
+      case _ => C4Some(((ina.headOption,inb.headOption),(ina.drop(1),inb.drop(1))))
+    }
+    unfold[(C4Option[A], C4Option[B]), (C5Stream[A], C5Stream[B])]((ina, inb))(ina_inb => next(ina_inb._1, ina_inb._2))
+  }
+
 }
--- src/test/scala/org/exercises/fpis/ch5/Chapter5Spec.scala
+++ src/test/scala/org/exercises/fpis/ch5/Chapter5Spec.scala
@@ -1,11 +1,13 @@
 package org.exercises.fpis.ch5
 
-import org.exercises.fpis.ch3.{C3Cons, C3Nil}
+import org.exercises.fpis.ch3.{C3Cons, C3List, C3Nil}
+import org.exercises.fpis.ch4.{C4None, C4Some}
 import org.specs2.mutable
 
 object Chapter5Spec  extends mutable.Specification {
 
   import C5Stream._
+  import org.exercises.fpis.ch3.Chapter3L
 
   val si1 = cons(3, C5Empty)
   val si2 = cons(2, si1)
@@ -26,28 +28,48 @@ object Chapter5Spec  extends mutable.Specification {
     }
   }
 
-  "take" should {
+  "takeL" should {
 
     "behave for 1,2,3 elements" in {
-      si1.take(1) must_=== ((C5Empty, li1))
-      si2.take(2) must_=== ((C5Empty, li2))
-      si3.take(3) must_=== ((C5Empty, li3))
+      si1.takeL(1) must_=== ((C5Empty, li1))
+      si2.takeL(2) must_=== ((C5Empty, li2))
+      si3.takeL(3) must_=== ((C5Empty, li3))
     }
     "behave for empty" in {
-      C5Empty.take(0) must_=== ((C5Empty, C3Nil))
+      C5Empty.takeL(0) must_=== ((C5Empty, C3Nil))
     }
     "behave for large n" in {
-      si2.take(20) must_=== ((C5Empty, li2))
+      si2.takeL(20) must_=== ((C5Empty, li2))
     }
 
     "behave for partial n" in {
-      val (left21, taken21) = si2.take(1)
+      val (left21, taken21) = si2.takeL(1)
       left21.toList must_=== C3Cons(3, C3Nil)
       taken21 must_=== C3Cons(2, C3Nil)
 
-      val (left31, taken31) = si3.take(1)
+      val (left31, taken31) = si3.takeL(1)
       left31.toList must_=== C3Cons(2, C3Cons(3, C3Nil))
       taken31 must_=== C3Cons(1, C3Nil)
+    }
+  }
+
+  "take" should {
+
+    "behave for 1,2,3 elements" in {
+      si1.take(1).toList must_=== li1
+      si2.take(2).toList must_=== li2
+      si3.take(3).toList must_=== li3
+    }
+    "behave for empty" in {
+      C5Empty.take(0) must_=== C5Empty
+    }
+    "behave for large n" in {
+      si2.take(20).toList must_=== li2
+    }
+
+    "behave for partial n" in {
+      si2.take(1).toList must_=== C3Cons(2, C3Nil)
+      si3.take(1).toList must_=== C3Cons(1, C3Nil)
     }
   }
 
@@ -70,4 +92,235 @@ object Chapter5Spec  extends mutable.Specification {
       si3.drop(1).toList must_=== C3Cons(2, C3Cons(3, C3Nil))
     }
   }
+
+  "takeWhile" should {
+
+    "behave for 2 elements" in {
+      val sample: C3List[Int] = si3.takeWhile(a => a <= 2).toList
+      sample must_=== C3Cons(1, C3Cons(2, C3Nil))
+    }
+    "behave for empty" in {
+      val e = C5Empty:C5Stream[Int]
+      e.takeWhile(_ => true) must_=== C5Empty
+    }
+  }
+
+  "forAll" should {
+
+    "behave for 2 elements" in {
+      si3.forAll(a => a <= 2) must beFalse
+      si3.forAll(a => a <= 3) must beTrue
+    }
+    "behave for empty" in {
+      val e = C5Empty:C5Stream[Int]
+      e.forAll(_ => false) must beTrue
+    }
+  }
+
+  "takeWhileR" should {
+
+    "behave for 2 elements" in {
+      val sample: C3List[Int] = si3.takeWhileR(a => a <= 2).toList
+      sample must_=== C3Cons(1, C3Cons(2, C3Nil))
+    }
+    "behave for empty" in {
+      val e = C5Empty:C5Stream[Int]
+      e.takeWhileR(_ => true) must_=== C5Empty
+    }
+  }
+
+  "headOption" should {
+
+    "behave for 1,2,3 elements" in {
+      si1.headOption must_=== C4Some(li1.head)
+      si2.headOption must_=== C4Some(li2.head)
+      si3.headOption must_=== C4Some(li3.head)
+    }
+    "behave for empty" in {
+      C5Empty.headOption must_=== C4None
+    }
+  }
+
+  "map" should {
+    import Chapter3L.c3map
+    def f(i: Int): String = i.toString
+
+    "behave for 1,2,3 elements" in {
+      si1.map(f).toList must_=== c3map(li1)(f)
+      si2.map(f).toList must_=== c3map(li2)(f)
+      si3.map(f).toList must_=== c3map(li3)(f)
+    }
+    "behave for empty" in {
+      C5Empty.map(f) must_=== C5Empty
+    }
+  }
+
+  "filter" should {
+    import Chapter3L.c3filter
+    def f(a: Int): Boolean = 0 == a%2
+
+    "remove all odds" in {
+      si1.filter(f).toList must_=== c3filter(li1)(f)
+      si2.filter(f).toList must_=== c3filter(li2)(f)
+      si3.filter(f).toList must_=== c3filter(li3)(f)
+    }
+    "behave for empty" in {
+      C5Empty.filter(f) must_=== C5Empty
+      C5Empty.filter(f) must_=== C5Empty
+    }
+  }
+
+  "append" should {
+    import Chapter3L.concat
+
+    "behave for 1,2,3 elements" in {
+      si1.append(si1).toList must_=== concat(li1,li1)
+      si2.append(si1).toList must_=== concat(li2,li1)
+      si3.append(si2).toList must_=== concat(li3,li2)
+    }
+    "behave no elements" in {
+      si1.append(C5Stream.empty[Int]).toList must_=== li1
+      si2.append(C5Stream.empty[Int]).toList must_=== li2
+      si3.append(C5Stream.empty[Int]).toList must_=== li3
+    }
+    "behave for empty" in {
+      C5Empty.append(si1).toList must_=== li1
+      C5Empty.append(si2).toList must_=== li2
+      C5Empty.append(si3).toList must_=== li3
+    }
+  }
+
+  "flatMap" should {
+    import Chapter3L.c3flatMap
+    def fs(i: Int): C5Stream[String] = C5Cons(() => i.toString,() => C5Empty)
+    def fl(i: Int): C3List[String] = C3Cons(i.toString,C3Nil)
+
+    "behave for 1,2,3 elements" in {
+      si1.flatMap(fs).toList must_=== c3flatMap(li1)(fl)
+      si2.flatMap(fs).toList must_=== c3flatMap(li2)(fl)
+      si3.flatMap(fs).toList must_=== c3flatMap(li3)(fl)
+    }
+    "behave for empty" in {
+      C5Empty.flatMap(fs) must_=== C5Empty
+    }
+  }
+
+  "constant" should {
+
+    "behave for 1,2,3 elements" in {
+      constant(1).take(1).toList must_=== C3Cons(1,C3Nil)
+      constant(1).take(2).toList must_=== C3Cons(1,C3Cons(1,C3Nil))
+      constant(1).take(3).toList must_=== C3Cons(1,C3Cons(1,C3Cons(1,C3Nil)))
+    }
+  }
+
+  "from" should {
+
+    "behave for 1,2,3 elements" in {
+      from(1).take(1).toList must_=== C3Cons(1,C3Nil)
+      from(1).take(2).toList must_=== C3Cons(1,C3Cons(2,C3Nil))
+      from(1).take(3).toList must_=== C3Cons(1,C3Cons(2,C3Cons(3,C3Nil)))
+    }
+  }
+
+  "fibNext" should {
+
+    "behave for 1,2,3, 6 elements" in {
+      // (0) 1 1 2 3 5 ... f(n+1) = f(n) + f(n-1)
+      fibSeqFrom1.headOption must_=== C4Some(1)
+      fibSeqFrom1.drop(1).headOption must_=== C4Some(1)
+      fibSeqFrom1.drop(2).headOption must_=== C4Some(2)
+      fibSeqFrom1.drop(3).headOption must_=== C4Some(3)
+      fibSeqFrom1.drop(4).headOption must_=== C4Some(5)
+    }
+  }
+
+  "unfoldOnes" should {
+    "persevere being 1" in {
+      unfoldOnes.take(0) must_=== C5Empty
+      unfoldOnes.take(1).headOption must_=== C4Some(1)
+      unfoldOnes.take(2).toList must_=== C3Cons(1, C3Cons(1, C3Nil))
+      unfoldOnes.take(10).headOption must_=== C4Some(1)
+    }
+  }
+
+  "unfoldConst" should {
+    val k = "foo"
+    "persevere being const" in {
+      unfoldConst(k).take(0) must_=== C5Empty
+      unfoldConst(k).take(1).headOption must_=== C4Some(k)
+      unfoldConst(k).take(2).toList must_=== C3Cons(k, C3Cons(k, C3Nil))
+      unfoldConst(k).take(10).headOption must_=== C4Some(k)
+    }
+  }
+
+  "unfoldFrom" should {
+
+    "behave for 1,2,3 elements" in {
+      unfoldFrom(1).take(1).toList must_=== C3Cons(1,C3Nil)
+      unfoldFrom(1).take(2).toList must_=== C3Cons(1,C3Cons(2,C3Nil))
+      unfoldFrom(1).take(3).toList must_=== C3Cons(1,C3Cons(2,C3Cons(3,C3Nil)))
+    }
+  }
+
+  "unfoldFib" should {
+
+    "behave for 1,2,3, 6 elements" in {
+      // (0) (1) 1 2 3 5 ... f(n+1) = f(n) + f(n-1)
+      unfoldFibFrom2.headOption must_=== C4Some(1)
+      unfoldFibFrom2.drop(1).headOption must_=== C4Some(2)
+      unfoldFibFrom2.drop(2).headOption must_=== C4Some(3)
+      unfoldFibFrom2.drop(3).headOption must_=== C4Some(5)
+      unfoldFibFrom2.drop(4).headOption must_=== C4Some(8)
+    }
+  }
+
+  "unfoldMap" should {
+    import Chapter3L.c3map
+    def f(i: Int): String = i.toString
+
+    "behave for 1,2,3 elements" in {
+      si1.unfoldMap(f).toList must_=== c3map(li1)(f)
+      si2.unfoldMap(f).toList must_=== c3map(li2)(f)
+      si3.unfoldMap(f).toList must_=== c3map(li3)(f)
+    }
+    "behave for empty" in {
+      C5Empty.unfoldMap(f) must_=== C5Empty
+    }
+  }
+
+  "unfoldTakeWhile" should {
+
+    "behave for 2 elements" in {
+      val sample: C3List[Int] = si3.unfoldTakeWhile(a => a <= 2).toList
+      sample must_=== C3Cons(1, C3Cons(2, C3Nil))
+    }
+    "behave for empty" in {
+      val e = C5Empty:C5Stream[Int]
+      e.unfoldTakeWhile(_ => true) must_=== C5Empty
+    }
+  }
+
+  "unfoldTake" should {
+
+    "behave for 1,2,3 elements" in {
+      si1.unfoldTake(1).toList must_=== li1
+      si2.unfoldTake(2).toList must_=== li2
+      si3.unfoldTake(3).toList must_=== li3
+    }
+    "behave for empty" in {
+      C5Empty.unfoldTake(0) must_=== C5Empty
+    }
+    "behave for large n" in {
+      si2.unfoldTake(20).toList must_=== li2
+    }
+
+    "behave for partial n" in {
+      si2.unfoldTake(1).toList must_=== C3Cons(2, C3Nil)
+      si3.unfoldTake(1).toList must_=== C3Cons(1, C3Nil)
+    }
+  }
+
+
+
 }
